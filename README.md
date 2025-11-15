2f612dca352217ee78478f74fae11d6d1d373104
kevinvalen12 <valenciakevin894@gmail.com>
2025-11-14 20:56:36 -0500

    feat(lesson-04): Creating-an-interface

--- README.md
+++ README.md
@@ -4,11 +4,11 @@
 2. ng serve
 3. ng generate component home
 ## es para que se cree a la carpeta
-4. ng generate interface interfaces/housinglocation = ng g i interfaces housinglocation
+4. ng generate interface interfaces/housinglocation
 
 ## link de conversaciones con IA:
 lesson-01, 02: https://chatgpt.com/share/6917313b-3064-8003-9377-2b9e950966ea
-lesson-03, 04: https://chatgpt.com/share/69178a07-4374-8003-8d64-eb9717fda2bf
+lesson-03, 04:  https://chatgpt.com/share/69178a07-4374-8003-8d64-eb9717fda2bf
 
 ## link de svg:
 https://icons8.com/icons/set/home
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 // importacion de componentes housinglocation
 import { HousingLocation } from '../housing-location/housing-location';
-//importar interface 
+//importar interfaz
 import type { HousingLocationInfo } from '../interfaces/housinglocation';
 
 // <app-housing-location />  home.html
@@ -14,19 +14,19 @@ import type { HousingLocationInfo } from '../interfaces/housinglocation';
 })
 
 export class Home {
-  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
-
-  housingLocation: HousingLocationInfo = {
-    id: 999,
-    name: 'Teste Home',
-    city: 'Test city',
-    state: 'ST',
-    photo: `${this.baseUrl}/example-house.png`,
-    availableUnits: 99,
-    wifi: true,
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';  
+  
+  housingLocation: HousingLocationInfo = {    
+    id: 9999,    
+    name: 'Test Home',    
+    city: 'Test city',    
+    state: 'ST',    
+    photo: `${this.baseUrl}/example-house.jpg`,    
+    availableUnits: 99,    
+    wifi: true,    
     laundry: false,
   }
 }
 
 // la leccion 3 explica como crear un componete y agregarle un filtro y boton a este componen
-// lecccion4 como se crea una interfaz para crear un dato para la aplicacion
+// lecccion 4 como se crea una interfaz para crear un dato 
